8c89ee6c79a2b9c036b7943ab7f5d04faed71223
Jordevim <jdeviamx2@gmail.com>
2021-09-18 16:01:28 -0500

    first commit

---
+++ src/components/Personajes.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+      <h1>EMPRENDETE</h1>
+
+      <v-carousel
+    cycle
+    height="400"
+    hide-delimiter-background
+    show-arrows-on-hover
+  >
+    <v-carousel-item
+      v-for="(slide, i) in slides"
+      :key="i"
+    >
+      <v-sheet
+        :color="colors[i]"
+        height="100%"
+      >
+        <v-row
+          class="fill-height"
+          align="center"
+          justify="center"
+        >
+          <div class="text-h2">
+            {{ slide }} Slide
+          </div>
+        </v-row>
+      </v-sheet>
+    </v-carousel-item>
+  </v-carousel>
+  </div>
+</template>
+
+<script>
+
+import store from '../store/index.js'
+
+export default {
+    data: ()=>{
+        return{
+
+        }
+    },
+    methods: {
+
+    },
+    created: ()=>{
+        store.dispatch('getPersonajes');
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/Login.vue
@@ -0,0 +1,56 @@
+<template>
+  <div id="contLogin">
+    
+    <v-card elevation="15">
+        <h2>Login</h2>
+
+      <v-card-text class="text--primary">
+        <v-form>
+          <v-text-field
+            label="Usuario"
+            placeholder="Usuario"
+            outlined
+          ></v-text-field>
+          <v-text-field
+            label="Contraseña"
+            placeholder="Contraseña"
+            outlined
+          ></v-text-field>
+        </v-form>
+      </v-card-text>
+
+      <v-card-actions>
+        <v-btn color="grey" text> Registrarse </v-btn>
+
+        <v-btn color="orange" text @click="login"> Iniciar Sesión </v-btn>
+      </v-card-actions>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+    data: ()=>{
+        return{
+
+        }
+    },
+    methods: {
+        login(){
+            console.log("Verificando credenciales...");
+            this.$router.push('/home');
+        }
+    }
+};
+</script>
+
+<style lang="scss">
+#contLogin {
+  width: 400px;
+  margin-left: 35%;
+  margin-top: 30vh;
+  h2{
+      text-align: center;
+  }
+}
+</style>
